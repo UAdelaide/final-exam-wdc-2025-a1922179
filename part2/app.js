467f89d1f12864a11b972e515434eac88baf3743
a1922179 <a1922179@adelaide.edu.au>
2025-06-20 14:43:17 +0000

    [automatic commit] 2025-06-20 14:43:16 UTC 220.253.74.211

--- part2/app.js
+++ part2/app.js
@@ -22,12 +22,10 @@ app.use(session({
 // Routes
 const walkRoutes = require('./routes/walkRoutes');
 const userRoutes = require('./routes/userRoutes');
-const dogRouter = require(path.resolve(__dirname, '../part1/routes/dog_service.js'));
 
 
 app.use('/api/walks', walkRoutes);
 app.use('/api/users', userRoutes);
-app.use('/api', dogRouter);
 
 // Export the app instead of listening here
 module.exports = app;
