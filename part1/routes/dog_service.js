fdc842074042232637c55fe65851ef6a6b73af0e
a1922179 <a1922179@adelaide.edu.au>
2025-06-20 06:32:32 +0000

    [automatic commit] 2025-06-20 06:32:31 UTC 220.253.74.211

--- part1/routes/dog_service.js
+++ part1/routes/dog_service.js
@@ -13,7 +13,7 @@ router.get('/walkrequests/open', async (req, res) => {
 });
 
 router.get('/walkers/summary', async (req, res) => {
-    const [rows] = await db.query('SELECT Users.username, ');
+    const [rows] = await db.query('SELECT Users.username, COUNT(');
     res.json(rows);
 });
 
