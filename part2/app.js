9d1f2a77bd006c19ee8469fc488685752003d5bd
a1922179 <a1922179@adelaide.edu.au>
2025-06-20 10:14:48 +0000

    [automatic commit] 2025-06-20 10:14:48 UTC 220.253.74.211

--- part2/app.js
+++ part2/app.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, '/public')));
 
 app.use(session({
-    secret: 'super', // stores in env file
+    secret: 'superSecretSession',
     resave: false,
     saveUninitialized: false,
     cookie: {
