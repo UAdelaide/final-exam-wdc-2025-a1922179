69db870172248ca3d106acbe20779900256e6df6
a1922179 <a1922179@adelaide.edu.au>
2025-06-20 04:10:41 +0000

    [automatic commit] 2025-06-20 04:10:41 UTC 220.253.74.211

--- part1/app.js
+++ part1/app.js
@@ -40,9 +40,16 @@ let db;
         name VARCHAR(50) NOT NULL,
         size ENUM('small', 'medium', 'large') NOT NULL,
         FOREIGN KEY (owner_id) REFERENCES Users(user_id)
-      )
+      );
 
-      
+      CREATE TABLE Users (
+        user_id INT AUTO_INCREMENT PRIMARY KEY,
+        username VARCHAR(50) UNIQUE NOT NULL,
+        email VARCHAR(100) UNIQUE NOT NULL,
+        password_hash VARCHAR(255) NOT NULL,
+        role ENUM('owner', 'walker') NOT NULL,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        );
     `);
 
 
