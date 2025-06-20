ca00dc8e29cec1d3b5956221d567023a19ce9473
a1922179 <a1922179@adelaide.edu.au>
2025-06-20 04:11:04 +0000

    [automatic commit] 2025-06-20 04:11:04 UTC 220.253.74.211

--- part1/app.js
+++ part1/app.js
@@ -32,7 +32,7 @@ let db;
       database: 'DogWalkService'
     });
 
-    // Create tables if it doesn't exist
+    // Create a table if it doesn't exist
     await db.execute(`
       CREATE TABLE IF NOT EXISTS Dogs (
         dog_id INT AUTO_INCREMENT PRIMARY KEY,
@@ -40,16 +40,7 @@ let db;
         name VARCHAR(50) NOT NULL,
         size ENUM('small', 'medium', 'large') NOT NULL,
         FOREIGN KEY (owner_id) REFERENCES Users(user_id)
-      );
-
-      CREATE TABLE Users (
-        user_id INT AUTO_INCREMENT PRIMARY KEY,
-        username VARCHAR(50) UNIQUE NOT NULL,
-        email VARCHAR(100) UNIQUE NOT NULL,
-        password_hash VARCHAR(255) NOT NULL,
-        role ENUM('owner', 'walker') NOT NULL,
-        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-        );
+      )
     `);
 
 
