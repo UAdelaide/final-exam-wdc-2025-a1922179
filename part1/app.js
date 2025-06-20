fc30c83a29b85b2e64ccbbccf054473652e5dd89
a1922179 <a1922179@adelaide.edu.au>
2025-06-20 04:14:44 +0000

    [automatic commit] 2025-06-20 04:14:43 UTC 220.253.74.211

--- part1/app.js
+++ part1/app.js
@@ -85,7 +85,7 @@ let db;
 
     // Create a table if it doesn't exist
     await db.execute(`
-      CREATE TABLE IF NOT EXISTS Dogs (
+      CREATE TABLE IF NOT EXISTS WalkRatings (
         rating_id INT AUTO_INCREMENT PRIMARY KEY,
         request_id INT NOT NULL,
         walker_id INT NOT NULL,
