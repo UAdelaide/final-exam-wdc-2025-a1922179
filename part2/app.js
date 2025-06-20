0b52f4e86b00409ad28dbcd758bda8311db049c1
a1922179 <a1922179@adelaide.edu.au>
2025-06-20 14:15:41 +0000

    [automatic commit] 2025-06-20 14:15:40 UTC 220.253.74.211

--- part2/app.js
+++ part2/app.js
@@ -8,7 +8,7 @@ const app = express();
 // Middleware
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '/public')));
-app.use('/api/dogs', require('./part1/routes/dogRoutes'));
+app.use('/api/dogs', require('./part1/routes/dog_service'));
 
 app.use(session({
     secret: 'superSecretSession',
