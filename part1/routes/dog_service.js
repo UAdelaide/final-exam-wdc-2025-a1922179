84fad25d31216cd78b088ae74170f036c35293ee
a1922179 <a1922179@adelaide.edu.au>
2025-06-20 05:42:25 +0000

    [automatic commit] 2025-06-20 05:42:25 UTC 220.253.74.211

--- part1/routes/dog_service.js
+++ part1/routes/dog_service.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-const db = require('./db');
+const db = require('.db');
 
 router.get('/dogs', async (req, res) => {
     const [rows] = await db.query('SELECT * FROM Dogs');
