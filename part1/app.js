0dc6118c6294e162086ea1e002e54ec840398a32
a1922179 <a1922179@adelaide.edu.au>
2025-06-20 13:21:19 +0000

    [automatic commit] 2025-06-20 13:21:18 UTC 220.253.74.211

--- part1/app.js
+++ part1/app.js
@@ -144,6 +144,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../part2')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
