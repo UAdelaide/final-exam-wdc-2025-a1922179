dfc208f47449917c44400baf41b1d099e6bd7e03
a1922179 <a1922179@adelaide.edu.au>
2025-06-20 04:21:53 +0000

    [automatic commit] 2025-06-20 04:21:53 UTC 220.253.74.211

--- part1/app.js
+++ part1/app.js
@@ -101,16 +101,16 @@ let db;
     `);
 
     // Insert data if table is empty
-    const [rows] = await db.execute('SELECT COUNT(*) AS count FROM Users');
-    if (rows[0].count === 0) {
+    const [rows1] = await db.execute('SELECT COUNT(*) AS count FROM Users');
+    if (rows1[0].count === 0) {
       await db.execute(`
         INSERT INTO Users (username, email, password_hash, role) VALUES ('alice123', 'alice@example.com', 'hashed123', 'owner'), ('bobwalker', 'bob@example.com', 'hashed456', 'walker'), ('carol123', 'carol@example.com', 'hashed789', 'owner'), ('elcampeon', 'campeon@example.com', 'hashed987', 'walker'), ('christina', 'christina@example.cpm', 'hashed654', 'owner')
       `);
     }
 
     // Insert data if table is empty
-    const [rows] = await db.execute('SELECT COUNT(*) AS count FROM Users');
-    if (rows[0].count === 0) {
+    const [rows2] = await db.execute('SELECT COUNT(*) AS count FROM Dogs');
+    if (rows2[0].count === 0) {
       await db.execute(`
         INSERT INTO Users (username, email, password_hash, role) VALUES ('alice123', 'alice@example.com', 'hashed123', 'owner'), ('bobwalker', 'bob@example.com', 'hashed456', 'walker'), ('carol123', 'carol@example.com', 'hashed789', 'owner'), ('elcampeon', 'campeon@example.com', 'hashed987', 'walker'), ('christina', 'christina@example.cpm', 'hashed654', 'owner')
       `);
