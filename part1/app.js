23f66569bea39ea426ca24098ffdbb06aa4034b8
a1922179 <a1922179@adelaide.edu.au>
2025-06-20 04:02:10 +0000

    [automatic commit] 2025-06-20 04:02:10 UTC 220.253.74.211

--- part1/app.js
+++ part1/app.js
@@ -34,7 +34,7 @@ let db;
 
     // Create a table if it doesn't exist
     await db.execute(`
-      CREATE TABLE IF NOT EXISTS books (
+      CREATE TABLE IF NOT EXISTS Dogs (
         id INT AUTO_INCREMENT PRIMARY KEY,
         title VARCHAR(255),
         author VARCHAR(255)
