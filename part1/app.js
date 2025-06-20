4e935b80b02723c956bfcde7a93bd8f172e2ede0
a1922179 <a1922179@adelaide.edu.au>
2025-06-20 03:59:56 +0000

    [automatic commit] 2025-06-20 03:59:56 UTC 220.253.74.211

--- part1/app.js
+++ part1/app.js
@@ -17,7 +17,7 @@ let db;
     const connection = await mysql.createConnection({
       host: 'localhost',
       user: 'root',
-      password: '' // Set your MySQL root password
+      password: 'mysql' // Set your MySQL root password
     });
 
     // Create the database if it doesn't exist
