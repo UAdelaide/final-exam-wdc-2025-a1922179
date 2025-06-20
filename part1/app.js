4c2bc56a293009cc479156a65cf994fc4b4d5db1
a1922179 <a1922179@adelaide.edu.au>
2025-06-20 04:23:00 +0000

    [automatic commit] 2025-06-20 04:23:00 UTC 220.253.74.211

--- part1/app.js
+++ part1/app.js
@@ -112,7 +112,7 @@ let db;
     const [rows2] = await db.execute('SELECT COUNT(*) AS count FROM Dogs');
     if (rows2[0].count === 0) {
       await db.execute(`
-        INSERT INTO Users (username, email, password_hash, role) VALUES ('alice123', 'alice@example.com', 'hashed123', 'owner'), ('bobwalker', 'bob@example.com', 'hashed456', 'walker'), ('carol123', 'carol@example.com', 'hashed789', 'owner'), ('elcampeon', 'campeon@example.com', 'hashed987', 'walker'), ('christina', 'christina@example.cpm', 'hashed654', 'owner')
+        INSERT INTO Dogs (owner_id, name, size) VALUES ((SELECT user_id FROM Users WHERE username = 'alice123'), 'Max', 'medium'), ((SELECT user_id FROM Users WHERE username = 'carol123'), 'Bella', 'small'), ((SELECT user_id FROM Users WHERE username = 'christina'), 'Protein', 'large'), ((SELECT user_id FROM Users WHERE username = 'christina'), 'Hotdog', 'small'), ((SELECT user_id FROM Users WHERE username = 'christina'), 'Princess', 'large')
       `);
     }
   } catch (err) {
